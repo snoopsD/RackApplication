fae2962d6dc2213f5ba03b60608f594fdae92381
DmitryP <snoops@list.ru>
2019-06-15 08:42:27 +0400

    some refactoring

--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
 require 'rack'
 require 'rack/response'
 
-DATE = DateTime.now
-FORMATS_TIME = { 'year' => DATE.year, 'month' => DATE.month,
-  'day' => DATE.day, 'hour' => DATE.hour,
-  'minute' => DATE.minute, 'second' => DATE.second }
-
 class App
 
-  def call(env)  
+  def call(env)
+    date = DateTime.now
+    @formats_time = { 'year' => date.year, 'month' => date.month,
+      'day' => date.day, 'hour' => date.hour,
+      'minute' => date.minute, 'second' => date.second }
     @request = Rack::Request.new(env)
-    check_request
+    flow
   end
 
   private 
@@ -27,16 +26,16 @@ class App
     @request.params['format'].split(',')
   end
 
-  def error_request
+  def not_found
     return [404, { 'Content-Type' => 'text/plain' }, ["Error request!\n"] ]
   end
 
   def valid_params
-    params_split.map { |format| FORMATS_TIME[format] }.compact.join('-')
+    params_split.map { |format| @formats_time[format] }.compact.join('-')
   end
 
   def invalid_params
-    params_split.reject { |format| FORMATS_TIME[format] }
+    params_split.reject { |format| @formats_time[format] }
   end
 
   def content_type
@@ -47,19 +46,13 @@ class App
     invalid_params.empty? ? 200 : 400 
   end
 
-  def check_request
-    if  method_get? && path_valid?
-      [status, content_type, body]
+  def flow
+    if !path_valid? || method_get?
+      not_found
+    elsif params_split.all? { |format| @formats_time.key?(format) }
+      [status, content_type, [valid_params]]
     else
-      error_request
-    end 
-  end
-
-  def body
-    if params_split.all? { |format| FORMATS_TIME.key?(format) }
-      [valid_params]
-    else 
-      ["Unknown time format #{invalid_params}"]
+      [status, content_type, ["Unknown time format #{invalid_params}"]]
     end
   end
 
